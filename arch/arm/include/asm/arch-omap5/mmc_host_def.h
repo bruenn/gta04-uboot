goldelico/letux-uboot

OMAP5: define HSMMC4 and HSMMC5 base
Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

arch/arm/include/asm/arch-omap5/mmc_host_def.h
@@ -34,5 +34,7 @@
 #define OMAP_HSMMC1_BASE	0x4809C100
 #define OMAP_HSMMC2_BASE	0x480B4100
 #define OMAP_HSMMC3_BASE	0x480AD100
+#define OMAP_HSMMC4_BASE	0x480D1100
+#define OMAP_HSMMC5_BASE	0x480D5100
 
 #endif /* MMC_HOST_DEF_H */

bruenn/gta04-uboot

arch/arm/include/asm/arch-omap5/mmc_host_def.h
@@ -34,5 +34,7 @@
 #define OMAP_HSMMC1_BASE	0x4809C000
 #define OMAP_HSMMC2_BASE	0x480B4000
 #define OMAP_HSMMC3_BASE	0x480AD000
+#define OMAP_HSMMC4_BASE	0x480D1000
+#define OMAP_HSMMC5_BASE	0x480D5000
 
 #endif /* MMC_HOST_DEF_H */