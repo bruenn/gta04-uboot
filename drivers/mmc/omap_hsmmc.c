goldelico/letux-uboot

omap hsmmc: define base_addr for HSMMC4 and HSMMC5
Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

drivers/mmc/omap_hsmmc.c
@@ -677,6 +677,18 @@ int omap_mmc_init(int dev_index, uint host_caps_mask, uint f_max, int cd_gpio,
 #endif
 		break;
 #endif
+#ifdef OMAP_HSMMC4_BASE
+	case 3:
+		priv_data->base_addr = (struct hsmmc *)OMAP_HSMMC4_BASE;
+		break;
+#endif
+#ifdef OMAP_HSMMC5_BASE
+	case 4:
+		priv_data->base_addr = (struct hsmmc *)OMAP_HSMMC5_BASE;
+		/* Enable 8-bit interface for eMMC on OMAP5 */
+		host_caps_val |= MMC_MODE_8BIT;
+		break;
+#endif
 	default:
 		priv_data->base_addr = (struct hsmmc *)OMAP_HSMMC1_BASE;
 		return 1;

bruenn/gta04-uboot

drivers/mmc/omap_hsmmc.c
@@ -1487,6 +1487,18 @@ int omap_mmc_init(int dev_index, uint host_caps_mask, uint f_max, int cd_gpio,
 #endif
 		break;
 #endif
+#ifdef OMAP_HSMMC4_BASE
+	case 3:
+		priv->base_addr = (struct hsmmc *)OMAP_HSMMC4_BASE;
+		break;
+#endif
+#ifdef OMAP_HSMMC5_BASE
+	case 4:
+		priv->base_addr = (struct hsmmc *)OMAP_HSMMC5_BASE;
+		/* Enable 8-bit interface for eMMC on OMAP5 */
+		host_caps_val |= MMC_MODE_8BIT;
+		break;
+#endif
 	default:
 		priv->base_addr = (struct hsmmc *)OMAP_HSMMC1_BASE;
 		return 1;